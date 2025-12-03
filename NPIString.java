b1ada6220fd3dba729e84cdaba948fc12739dbf2
GAUDRY BEN p2506224 <p2506224@b301pcj13.univ-lyon1.fr>
2025-12-03 15:33:25 +0100

    avancements

--- NPIString.java
+++ NPIString.java
@@ -8,65 +8,59 @@ public class NPIString
         extends AbstractParseTreeVisitor<Void>
         implements NPIVisitor<Void> {
 
-    private String str = "";
     private Stack<Integer> stack;
 
-    private int curr_reg = 0;
-
-    public NPIString() {
-        this.stack = new Stack<Integer>();
-    }
-
+    public NPIString() {}
 
     @Override
     public Void visitEntier(NPIParser.EntierContext ctx) {
-        System.out.println("XOR R0 R0 R0");
-        System.out.println("ADDi R0 R0 " + ctx.getText());
-        System.out.println("ADDi R2 R2 1");
-        System.out.println("ST R0 R2");
-
+        System.out.println("ADDi R0 R0 1");
+        System.out.println("XOR R1 R1 R1");
+        System.out.println("ADDi R1 R1 " + ctx.getText());
+        System.out.println("LD R1 R0");
         return null;
     }
 
     @Override
     public Void visitMain(NPIParser.MainContext ctx) {
-        System.out.println("XOR R2 R2 R2");
+        System.out.println("XOR R0 R0 R0");
         visit(ctx.getChild(0));
-        System.out.println("LD R0 R2");
-        System.out.println("SUBi R2 R2 1");
-        System.out.println("OUT R0");
-        System.out.println("STOP");
+        System.out.println("LD R1 R0");
+        System.out.println("SUBi R0 R0 1");
+        System.out.println("PRINT R1");
         return null;
     }
-    
+
     @Override
     public Void visitOperation(NPIParser.OperationContext ctx) {
         visit(ctx.getChild(0));
         visit(ctx.getChild(1));
 
-        System.out.println("LD R1 R2");
-        System.out.println("SUBi R2 R2 1");
+        String op = "";
 
-        System.out.println("LD R0 R2");
-        System.out.println("SUBi R2 R2 1");
+        Integer b = stack.pop();
+        Integer a = stack.pop();
+        Integer res = 0;
 
         switch (ctx.getChild(2).getText()) {
             case "-":
-                System.out.println("SUB R0 R1 R0");
+                op = "SUB";
                 break;
             case "+":
-                System.out.println("ADD R0 R1 R0");
+                op = "ADD";
                 break;
             case "*":
-                System.out.println("MUL R0 R1 R0");
+                op = "MUL";
                 break;
             case "/":
-                System.out.println("DIV R0 R1 R0");
+                op = "DIV";
                 break;
             case "%":
-                System.out.println("MOD R0 R1 R0");
+                op = "MOD";
                 break;
         }
+
+        stack.push(res);
 
         return null;
     }
